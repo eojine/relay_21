fa7f07f1a18693421a80d55606a369ca1e3eb55a
Kyungyoung Heo <bbvch13531@gmail.com>
2020-08-07 18:20:54 +0900

    Update
    Signed-off-by: Kyungyoung Heo <bbvch13531@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import badwords
 import nltk
 nltk.download('punkt')
 from flask import Flask, Response, request, jsonify
 from nltk.tokenize import word_tokenize
-import badwords
+from flask_cors import  CORS, cross_origin
 app = Flask(__name__)
 app.config["RELEASE"] = True
+CORS(app)
 @app.route('/hello', methods=['POST'])
 def home():
     data = request.get_json()
@@ -16,6 +18,16 @@ def home():
         "sentence":sentence
     }
     return jsonify(data)# 받아온 데이터를 다시 전송
+@app.route('/report', methods=['POST'])
+def report():
+    data = request.get_json()
+    report_word = data.get("report")
+    badwords.badwords.append(report_word)
+    # print(len(badwords.badwords))
+    data = {
+        "result":"감사합니다."
+    }
+    return jsonify(data)
 def profanityAnalysis(input):
     word_tokens = word_tokenize(input)
     result = []
@@ -25,4 +37,4 @@ def profanityAnalysis(input):
         else:
             result.append("*" * len(w))
     return "".join(result)
-app.run(host= '0.0.0.0', port=5000, debug=False)
+app.run(host= '0.0.0.0', port=80, debug=True)
